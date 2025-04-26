6bb075f64441807a5b1b5a7d685bc0bbda25dfa0
Xespia <fernando.olguinv1@gmail.com>
2025-04-26 02:20:41 -0400

    Done

---
+++ app/src/main/java/com/example/eva1/WelcomeActivity.java
@@ -0,0 +1,29 @@
+package com.example.eva1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    Button btnVolver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        btnVolver = findViewById(R.id.btnVolver);
+
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Opcional: para cerrar esta activity
+            }
+        });
+    }
+}
